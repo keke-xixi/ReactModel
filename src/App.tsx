eb68755c621aa8e110a596ffc94999da43eb2a73
CNVITP69 <Zz123@123456>
2026-05-22 18:04:25 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
-import zhCN from 'antd/locale/zh_CN';
-import { appTheme } from './theme/antdTheme';
 import Login from './pages/login';
 import AppLayout from './layout/Layout';
 import Dashboard from './pages/dashboard';
 import Report from './pages/report';
-import ReportDesign from './pages/report/design';
-import ReportPreview from './pages/report/preview';
 import SystemParams from './pages/system/params';
-import SystemMenu from './pages/system/menu';
-import SystemDict from './pages/system/dict';
 import Setting from './pages/setting';
 import Knowledge from './pages/knowledge';
 import Tool from './pages/tool';
 
 function App() {
     return (
-        <ConfigProvider locale={zhCN} theme={appTheme}>
+        <ConfigProvider>
             <BrowserRouter>
                 <Routes>
                     <Route path="/login" element={<Login />} />
@@ -26,11 +20,7 @@ function App() {
                         <Route index element={<Dashboard />} />
                         <Route path="home" element={<Dashboard />} />
                         <Route path="report" element={<Report />} />
-                        <Route path="report/design/:id" element={<ReportDesign />} />
-                        <Route path="report/preview/:id" element={<ReportPreview />} />
                         <Route path="system-params" element={<SystemParams />} />
-                        <Route path="system-menu" element={<SystemMenu />} />
-                        <Route path="system-dict" element={<SystemDict />} />
                         <Route path="setting" element={<Setting />} />
                         <Route path="tool" element={<Tool />} />
                         <Route path="knowledge" element={<Knowledge />} />
